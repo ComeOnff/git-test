03fdc89f9519891f981068e5ff0ed204f9a9be4c
1513743371@qq.comm <1513743371@qq.com>
2022-03-08 09:02:49 +0800

    first commit

---
+++ src/main/java/com/hunan/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.hunan.git;
+
+/**
+ * @author 猫咚
+ * @date 2022/3/8 - 8:59
+ */
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
